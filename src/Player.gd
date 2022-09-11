3f30fcd93a9625b1c485d25ab73bb3a5d328e724
Matt <biscuit.substantial@gmail.com>
2022-09-11 18:28:39 +0100

    Added player character
    Added player characted with temporary art.

---
+++ src/Player.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+
+export(int) var MAX_SPEED = 100
+
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = 1
+	else:
+		velocity.x = 0
+		
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = 1
+	else:
+		velocity.y = 0
+		
+	# Normilize to max speed
+	velocity = velocity.normalized() * MAX_SPEED
+	
+	move_and_slide(velocity)
+
